7c8e597240fce0e8921ae2077bf5cf1fa274223c
viniciusbp7 <vinicius.b.pomilio@gmail.com>
2025-03-09 21:47:49 -0300

    exs 2 teste

--- exe2/main.c
+++ exe2/main.c
@@ -20,7 +20,30 @@ void btn_callback(uint gpio, uint32_t events) {
     }
 }
 
+bool timer_callback_red(repeating_timer_t *rt) {
+    if (gpio_get(LED_PIN_R)) {
+        gpio_put(LED_PIN_R, 0);
+    } else {
+        gpio_put(LED_PIN_R, 1);
+    }
+    return true;  
+}
+bool timer_callback_green(repeating_timer_t *rt) {
+    if (gpio_get(LED_PIN_G)) {
+        gpio_put(LED_PIN_G, 0);
+    } else {
+        gpio_put(LED_PIN_G, 1);
+    }
+    return true;  
+}
+
 int main() {
+
+    int timer_red_active = 0;
+    repeating_timer_t timer_red;
+    int timer_green_active = 0;
+    repeating_timer_t timer_green;
+
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
 
@@ -35,18 +58,34 @@ int main() {
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
 
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,&btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
     while (true) {
 
         if (flag_r) {
             flag_r = 0;
+            if (!timer_red_active) {
+                if (add_repeating_timer_ms(500, timer_callback_red, NULL, &timer_red)) {
+                    timer_red_active = true;  
+                }
+            } else {
+                gpio_put(LED_PIN_R, 0);
+                cancel_repeating_timer(&timer_red);
+                timer_red_active = false;
+            }
         }
-
         if (flag_g) {
             flag_g = 0;
+            if (!timer_green_active) {
+                if (add_repeating_timer_ms(250, timer_callback_green, NULL, &timer_green)) {
+                    timer_green_active = true;  
+                }
+            } else {
+                gpio_put(LED_PIN_G, 0);
+                cancel_repeating_timer(&timer_green);
+                timer_green_active = false;
+            }
         }
     }
 }
