fdf8a4967bd9bab4245460d9436c08ae035cb092
viniciusbp7 <vinicius.b.pomilio@gmail.com>
2025-03-09 22:16:15 -0300

    testando o exs 3

--- exe3/main.c
+++ exe3/main.c
@@ -6,6 +6,8 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile bool timer_fired = false;
+volatile int flag_alarm = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
@@ -15,9 +17,19 @@ void btn_callback(uint gpio, uint32_t events) {
     } else if (events == 0x8) { // rise edge
         if (gpio == BTN_PIN_R)
             flag_f_r = 0;
+            flag_alarm=1;
     }
 }
-
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    if(flag_alarm){
+        timer_fired = false;
+        return 0;
+    }
+    else{
+        timer_fired = true;
+        return 0;
+    }
+}
 int main() {
     stdio_init_all();
 
@@ -28,12 +40,19 @@ int main() {
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
 
-    gpio_set_irq_enabled_with_callback(
-        BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
-
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
     while (true) {
 
         if (flag_f_r) {
+            flag_f_r = 0;
+            alarm_id_t alarm = add_alarm_in_ms(500, alarm_callback, NULL, false);
+            flag_alarm=0;
+        }
+        if (timer_fired) {
+            gpio_put(LED_PIN_R, 1);
+            sleep_ms(500);
+            gpio_put(LED_PIN_R, 0);
+            timer_fired = false;
         }
     }
 }
